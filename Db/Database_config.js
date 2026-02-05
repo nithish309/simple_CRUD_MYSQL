3e9dcb130fa5476603e750eacd2a356b39d69b4a
nithish309 <nithishchinnasamy91@gmail.com>
2026-02-05 16:05:33 +0530

    hello

--- Db/Database_config.js
+++ Db/Database_config.js
@@ -5,15 +5,11 @@ dotenv.config();
 
 const database=mysql.createPool({
 
-    user:process.env.SQL_USER,
-
-    host:process.env.SQL_HOST,
-
-    password:process.env.SQL_PASSWORD,
-
-    port :process.env.SQL_PORT,
-
-    database:process.env.SQL_DB,
+     host: process.env.SQL_HOST,
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DB,
+  port: process.env.SQL_PORT,
 
 })
 
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import db_conn from "./Db/Database_conn.js";
 import express from 'express';
 import dotenv from 'dotenv';
+dotenv.config();
 
 import userRoute from "./Routes/userRoute.js";
 
 const app=express();
 app.use(express.json());
 
-dotenv.config();
+
 
 const port=process.env.PORT;
 
